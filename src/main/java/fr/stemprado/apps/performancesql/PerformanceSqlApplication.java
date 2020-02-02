451a8d7573ae1a8f80bf874c74871fad4ec511c0
Sébastien Temprado <stemprado@gmail.com>
2020-02-02 20:41:47 +0100

    init springboot REST + JPA

---
+++ src/main/java/fr/stemprado/apps/performancesql/PerformanceSqlApplication.java
@@ -0,0 +1,13 @@
+package fr.stemprado.apps.performancesql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/stemprado/apps/performancesql/PerformanceSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.stemprado.apps.performancesql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
